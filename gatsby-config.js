f570897ab08dd4ca5489e26e9f7345dcccdd1003
manju <manju.thomas083@gmail.com>
2020-02-27 15:53:22 -0500

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
+    title: `Manju's Apprenticeship Blog`,
+    author: `Manju Thomas`,
+    description: `A blog during Manju's apprenticeship.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `kudukacar`,
     },
   },
   plugins: [
